004fbe36ef0174fd9f295239b59e94e81ab9c01a
Reuel John <reueljohn@gmail.com>
2018-11-17 12:57:08 +0530

    Added publisher field, get and set functions. Also added this refs and fixed OCD indentation

--- src/objects/Book.java
+++ src/objects/Book.java
@@ -6,62 +6,72 @@ public class Book {
 	private String genre;
 	private String ISBN;
 	private String ID;
+	private String publisher;
 	private int quantity;
 	private int available;
 
 	public String getTitle() {
-	return this.title;
+		return this.title;
 	}
 
 	public void setTitle(String title) {
-	this.title = title;
+		this.title = title;
 	}
 
 	public String getAuthor() {
-	return author;
+		return this.author;
 	}
 
 	public void setAuthor(String author) {
-	this.author = author;
+		this.author = author;
 	}
 
 	public String getGenre(){
-	return genre;
+		return this.genre;
 	}
 
 	public void setGenre(String genre){
-	this.genre=genre;
+		this.genre=genre;
 	}
 
 	public String getISBN() {
-		return ISBN;
+		return this.ISBN;
 	}
 
 	public void setISBN(String ISBN) {
-	this.ISBN=ISBN;
+		this.ISBN=ISBN;
 	}
 		
 	public String getid() {
-	return ID;
+		return this.ID;
 	}
 
 	public void setid(String ID) {
-	this.ID = ID;
+		this.ID = ID;
 	}
 
 	public int getQuantity() {
-	return quantity;
+		return this.quantity;
 	}
 
 	public void setQuantity(int quantity) {
-	this.quantity = quantity;
+		this.quantity = quantity;
 	}
 
 	public int getAvailable() {
-	return available;
+		return this.available;
 	}
+	
 	public void setAvailable(int available) {
-	this.available = available;
+		this.available = available;
+	}
+	
+	public void setPublisher(String publisher) {
+		this.publisher=publisher;
+	}
+	
+	public String getPublisher() {
+		return this.publisher;
 	}
 
 
