2a940494ba9b4739ca2994151797d041df19e9a6
Reuel John <reueljohn@gmail.com>
2018-11-22 13:12:27 +0530

    Added hasBorrowedBook(book,user)

--- src/dao/DBConnector.java
+++ src/dao/DBConnector.java
@@ -59,10 +59,29 @@ public class DBConnector {
 		return user;
 	}
 	
-	List<Book> browseBooks() {
+	boolean hasThisBook(User user,Book book) {
+		
+		List<Object[]> books = getUserCurrentIssues(user.getMemId());
+		
+		if(books.isEmpty()) return false;
+		
+		for(int i=0;i<books.size();i++) {
+			
+			Object[] array =books.get(i);
+			
+			if(book.getid()==(int)array[0]) {
+				return true;
+			}
+		}
+		return false;
+		
+	}
+	
+	List<Book> browseBooks(User user) {
 		
 		Connection conn;
 		List<Book> books = new ArrayList<Book>();
+		
 		try {
 			conn = dbUtil.getConnection();
 			 String query = "SELECT * FROM books";		        
@@ -198,7 +217,7 @@ public class DBConnector {
 	}
 	
 	
-	List<Object[]> getUserCurrentIssue(String user_id){
+	List<Object[]> getUserCurrentIssues(int user_id){
 		
 		List<Object[]> issues = new ArrayList<Object[]>();
 		
@@ -214,7 +233,7 @@ public class DBConnector {
 		    while(userSet.next()) {
 		    	
 		    	Object [] issueData = new Object[3];
-		    	issueData[0]=userSet.getString("book_id");
+		    	issueData[0]=userSet.getInt("book_id");
 		    	issueData[1]=userSet.getDate("issue_date");
 		    	issueData[2]=userSet.getDate("due_date");
 		    	issues.add(issueData);
