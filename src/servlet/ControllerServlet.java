74880a9471c8bacade8180d8c876c8eaa298385e
Nandini18 <nandini.agrawal03@gmail.com>
2018-12-04 19:30:55 +0530

    Logout

--- src/servlet/ControllerServlet.java
+++ src/servlet/ControllerServlet.java
@@ -89,7 +89,8 @@ public class ControllerServlet extends HttpServlet {
 	 */
 
 
-    	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+    	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException
+    	{
             HttpServletResponse response = (HttpServletResponse) res;
             response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
             response.setHeader("Pragma", "no-cache"); // HTTP 1.0.
